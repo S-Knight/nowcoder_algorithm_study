6bb838ae47e81672bbc736ccf9883b28af422852
knight <360776816@qq.com>
2020-07-14 20:52:34 +0800

    完善注释 完成冒泡排序

--- 第二章/课后练习2/src/com/igeekspace/Main.java
+++ 第二章/课后练习2/src/com/igeekspace/Main.java
@@ -3,6 +3,13 @@ package com.igeekspace;
 import java.util.Scanner;
 
 public class Main {
+    /**
+     * 交换数组两个元素的值
+     *
+     * @param chars 要交换的数组
+     * @param i     要交换元素1的索引
+     * @param j     要交换元素2的索引
+     */
     private static void swapChar(char[] chars, int i, int j) {
         if (i == j) {
             return;
@@ -13,6 +20,11 @@ public class Main {
         chars[j] = temp;
     }
 
+    /**
+     * 插入排序
+     *
+     * @param chars 要排序的数组
+     */
     private static void insertSort(char[] chars) {
         for (int i = 1; i < chars.length; i++) {
             //非英文字母的其它字符保持原来的位置。
@@ -36,6 +48,55 @@ public class Main {
         }
     }
 
+    private static void bubbleSort(char[] chars) {
+        int firstLetterIndex = findNextLetter(chars, 0);
+        int secondLetterIndex = findNextLetter(chars, firstLetterIndex + 1);
+
+        for (int i = 0; i < chars.length; i++) {
+            //非英文字母的其它字符保持原来的位置。
+            if (!Character.isLetter(chars[chars.length - 1 - i])) {
+                continue;
+            }
+
+            int j = firstLetterIndex;
+            int k = secondLetterIndex;
+
+            while (true) {
+                if (j == -1 || k == -1 || k >= chars.length - i) {
+                    break;
+                }
+
+                if (Character.toLowerCase(chars[j]) > Character.toLowerCase(chars[k])) {
+                    swapChar(chars, j, k);
+                }
+
+                j = k;
+                k = findNextLetter(chars, k + 1);
+            }
+        }
+    }
+
+    /**
+     * 查找从指定位置开始，字符数组里是英文字母的元素的索引
+     *
+     * @param chars 要查找的数组
+     * @param start 起始索引
+     * @return 如果能找到，则返回英文字母的元素的索引，否则返回-1
+     */
+    private static int findNextLetter(char[] chars, int start) {
+        if (start < 0) {
+            start = 0;
+        }
+
+        for (int i = start; i < chars.length; i++) {
+            if (Character.isLetter(chars[i])) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
     private static void printChars(char[] chars) {
         for (int i = 0; i < chars.length; i++) {
             System.out.print(chars[i]);
@@ -54,7 +115,10 @@ public class Main {
             char[] charArray = input.toCharArray();
 
             //插入排序
-            insertSort(charArray);
+            //insertSort(charArray);
+
+            //冒泡排序
+            bubbleSort(charArray);
 
             printChars(charArray);
         }
