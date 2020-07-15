9428aeefa5ec18230a5e60b33a36065340ca9ccf
knight <360776816@qq.com>
2020-07-15 19:32:42 +0800

    完成开班测评题目四：最长公共连续子串

---
+++ 00开班测评/最长公共连续子串/java/src/com/igeekspace/Main.java
@@ -0,0 +1,36 @@
+package com.igeekspace;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String string1 = scanner.nextLine();
+        String string2 = scanner.nextLine();
+
+        //让string1成为较短的那一串字符串
+        if (string2.length() < string1.length()) {
+            String temp = string1;
+            string1 = string2;
+            string2 = temp;
+        }
+
+        int largestCommonLength = 0;
+        for (int i = 0; i < string1.length(); i++) {
+            for (int j = i + 1; j <= string1.length(); j++) {
+                if (j - i <= largestCommonLength) {
+                    continue;
+                }
+
+                String substring = string1.substring(i, j);
+
+                if (string2.contains(substring)) {
+                    largestCommonLength = Math.max(substring.length(), largestCommonLength);
+                }
+            }
+        }
+
+        System.out.println(largestCommonLength);
+    }
+}
